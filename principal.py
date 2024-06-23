c73591a218b65c81f84235d32976f5c84b5e1b60
Garo <94136201+MagoGaro@users.noreply.github.com>
2024-06-22 21:06:31 -0300

    Add files via upload

--- principal.py
+++ principal.py
@@ -1,15 +1,14 @@
 import tkinter as tk
-from cliente.vista import Frame, barrita_menu
+from vistas.inicio import Frame2, barrita_menu
 
 def main():
     ventana = tk.Tk()
-    ventana.title('Listado Peliculas')
-    ventana.iconbitmap('img/videocamara.ico')
+    ventana.title('Gestor Autos')
+    ventana.iconbitmap('img/autito.ico')
     ventana.resizable(0,0)
 
     barrita_menu(ventana)
-    app = Frame(root = ventana)
-    
+    app = Frame2(root = ventana)
 
     ventana.mainloop()
 
