7db614a4bfc3f0c3d87e7afeaeae12743fb2d221
MagoGaro <gabriel.magogaro@gmail.com>
2024-06-29 16:43:05 -0300

    update_transacciones

--- vistas/transacciones.py
+++ vistas/transacciones.py
@@ -12,13 +12,11 @@ def open_vista_transacciones(root):
     vista_transacciones.resizable(0,0)
 
     # Add widgets to the secondary window here
-    app = Frame3(root = vista_transacciones)
+    app = Frame10(root = vista_transacciones)
+    app.config(background='#F7F9AF')
 
-    # Create a button to close the secondary window
-    close_button = tk.Button(vista_transacciones, text="Close", command=vista_transacciones.destroy)
-    close_button.pack()
 
-class Frame3(Frame):
+class Frame10(Frame):
     def __init__(self, root = None):
         super().__init__(root)
         self.root = root
@@ -27,29 +25,30 @@ class Frame3(Frame):
 
         self.label_form()
         self.input_form()
-        #self.tabla_cliente()
+        self.botones_principales()
+        self.tabla_transaccion()
 
     def label_form(self):
 
         self.label_id_de_vehículo = tk.Label(self, text="ID de Vehículo:")
         self.label_id_de_vehículo.config(font=('Arial',12,'bold'),background='#F7F9AF')
-        self.label_id_de_vehículo.grid(row= 1, column=3,padx=10,pady=10)
+        self.label_id_de_vehículo.grid(row= 1, column=0,padx=10,pady=10)
 
         self.label_id_de_cliente = tk.Label(self, text="ID de Cliente:")
         self.label_id_de_cliente.config(font=('Arial',12,'bold'),background='#F7F9AF')
-        self.label_id_de_cliente.grid(row= 2, column=0,padx=10,pady=10)
+        self.label_id_de_cliente.grid(row= 1, column=3,padx=10,pady=10)
 
-        self.label_tipo_de_transaccion = tk.Label(self, text="Tipo de Transaccion(Compra/Venta):")
+        self.label_tipo_de_transaccion = tk.Label(self, text="Tipo:")
         self.label_tipo_de_transaccion.config(font=('Arial',12,'bold'),background='#F7F9AF')
-        self.label_tipo_de_transaccion.grid(row= 2, column=3,padx=10,pady=10)
+        self.label_tipo_de_transaccion.grid(row= 2, column=0,padx=10,pady=10)
 
         self.label_fecha = tk.Label(self, text="Fecha:")
         self.label_fecha.config(font=('Arial',12,'bold'),background='#F7F9AF')
-        self.label_fecha.grid(row= 3, column=0,padx=10,pady=10)
+        self.label_fecha.grid(row= 2, column=3,padx=10,pady=10)
 
         self.label_Monto = tk.Label(self, text="Monto:")
         self.label_Monto.config(font=('Arial',12,'bold'),background='#F7F9AF')
-        self.label_Monto.grid(row= 3, column=3,padx=10,pady=10)
+        self.label_Monto.grid(row= 3, column=0,padx=10,pady=10)
 
         self.label_observaciones = tk.Label(self, text="Observaciones:")
         self.label_observaciones.config(font=('Arial',12,'bold'),background='#F7F9AF')
@@ -59,54 +58,97 @@ class Frame3(Frame):
 
         self.id_de_vehículo_c = tk.StringVar()
         self.entry_id_de_vehículo= tk.Entry(self,textvariable=self.id_de_vehículo_c)
-        self.entry_id_de_vehículo.config(width=20, font=('Arial',12))
-        self.entry_id_de_vehículo.grid(row= 1, column=4,padx=10,pady=10, columnspan='2')
+        self.entry_id_de_vehículo.config(width=20, font=('Arial',12),state='disabled')
+        self.entry_id_de_vehículo.grid(row= 1, column=1,padx=10,pady=10, columnspan='2')
 
         self.id_de_cliente_c = tk.StringVar()
         self.entry_id_de_cliente = tk.Entry(self,textvariable=self.id_de_cliente_c)
-        self.entry_id_de_cliente.config(width=20, font=('Arial',12))
-        self.entry_id_de_cliente.grid(row= 2, column=1,padx=10,pady=10, columnspan='2')
+        self.entry_id_de_cliente.config(width=20, font=('Arial',12),state='disabled')
+        self.entry_id_de_cliente.grid(row= 1, column=4,padx=10,pady=10, columnspan='2')
 
         self.tipo_transaccion_c = tk.StringVar()
         self.entry_tipo_de_transaccion = tk.Entry(self,textvariable=self.tipo_transaccion_c)
-        self.entry_tipo_de_transaccion.config(width=20, font=('Arial',12))
-        self.entry_tipo_de_transaccion.grid(row= 2, column=4,padx=10,pady=10, columnspan='2')
+        self.entry_tipo_de_transaccion.config(width=20, font=('Arial',12),state='disabled')
+        self.entry_tipo_de_transaccion.grid(row= 2, column=1,padx=10,pady=10, columnspan='2')
 
         self.fecha_c = tk.StringVar()
         self.entry_fecha = tk.Entry(self,textvariable=self.fecha_c)
-        self.entry_fecha.config(width=25, font=('Arial',12))
-        self.entry_fecha.grid(row= 3, column=1,padx=10,pady=10, columnspan='2')
+        self.entry_fecha.config(width=25, font=('Arial',12),state='disabled')
+        self.entry_fecha.grid(row= 2, column=4,padx=10,pady=10, columnspan='2')
 
         self.monto_c = tk.StringVar()
         self.entry_monto = tk.Entry(self,textvariable=self.monto_c)
-        self.entry_monto.config(width=15, font=('Arial',12))
-        self.entry_monto.grid(row= 3, column=3,padx=10,pady=10, columnspan='4')
+        self.entry_monto.config(width=15, font=('Arial',12),state='disabled')
+        self.entry_monto.grid(row= 3, column=1,padx=10,pady=10, columnspan='4')
 
         self.observaciones_c = tk.StringVar()
         self.entry_observaciones = tk.Entry(self,textvariable=self.observaciones_c)
-        self.entry_observaciones.config(width=50, font=('Arial',12))
+        self.entry_observaciones.config(width=50, font=('Arial',12),state='disabled')
         self.entry_observaciones.grid(row= 4, column=1,padx=10,pady=10, columnspan='4')
     
-    def tabla_cliente(self):
-        self.tabla = ttk.Treeview(self, columns=('ID de Transaccion','ID de Vehículo','ID de Transacción','Fecha','Monto','Observaciones'))
-        self.tabla.grid(row=5,column=0,columnspan=6,sticky='nse')
+    def botones_principales(self):
+        self.btn_alta = tk.Button(self, text='Nuevo',command= self.habilitar_campos)
+        self.btn_alta.config(width= 20,font=('Arial', 12,'bold'),fg ='#FFFFFF' , bg='#1C500B',cursor='hand2',activebackground='#3FD83F',activeforeground='#000000')
+        self.btn_alta.grid(row= 5, column=0,padx=10,pady=10, columnspan='2')
+
+        self.btn_modi = tk.Button(self, text='Guardar',command= self.guardar_campos)
+        self.btn_modi.config(width= 20,font=('Arial', 12,'bold'),fg ='#FFFFFF' , bg='#0D2A83',cursor='hand2',activebackground='#7594F5',activeforeground='#000000',state='disabled')
+        self.btn_modi.grid(row= 5, column=2,padx=10,pady=10, columnspan='2')
+
+        self.btn_cance = tk.Button(self, text='Cancelar', command= self.bloquear_campos)
+        self.btn_cance.config(width= 20,font=('Arial', 12,'bold'),fg ='#FFFFFF' , bg='#A90A0A',cursor='hand2',activebackground='#F35B5B',activeforeground='#000000',state='disabled')
+        self.btn_cance.grid(row= 5, column=4,padx=10,pady=10, columnspan='2')
+
+    def habilitar_campos(self):
+        self.entry_id_de_vehículo.config(state='normal')
+        self.entry_id_de_cliente.config(state='normal')
+        self.entry_fecha.config(state='normal')
+        self.entry_observaciones.config(state='normal')
+        self.entry_monto.config(state='normal')
+        self.btn_modi.config(state='normal')
+        self.btn_cance.config(state='normal')
+        self.btn_alta.config(state='disabled')
+
+    def bloquear_campos(self):
+        self.entry_id_de_vehículo.config(state='disabled')
+        self.entry_id_de_cliente.config(state='disabled')
+        self.entry_fecha.config(state='disabled')
+        self.entry_monto.config(state='disabled')
+        self.entry_observaciones.config(state='disabled')
+
+        self.monto_c.set('')
+        self.observaciones_c.set('')
+        self.fecha_c.set('')
+        self.tipo_transaccion_c.set('')
+        self.id_de_cliente_c.set('')
+        self.id_de_vehículo_c.set('')
+
+        self.btn_modi.config(state='disabled')
+        self.btn_cance.config(state='disabled')
+        self.btn_alta.config(state='normal')
+
+
+    def tabla_transaccion(self):
+        self.tabla = ttk.Treeview(self, columns=('ID de Vehículo','ID de Cliente','Tipo de Transaccion','Fecha','Monto','Observaciones'))
+        self.tabla.grid(row=6,column=0,columnspan=6,sticky='nse')
 
         self.scroll = tk.Scrollbar(self, orient='vertical', command=self.tabla.yview)
-        self.scroll.grid(row=5, column=4,sticky='nse')
+        self.scroll.grid(row=6, column=6,sticky='nse')
         self.tabla.configure(yscrollcommand= self.scroll.set)
         
-        for p in self.contenido_cli:
+        self.contenido_t = leer_transacciones('transacciones')
+        self.contenido_t.reverse()
+        for p in self.contenido_t:
             self.tabla.insert('',0,text=p['id_transaccion'],
-                              values = (p['nombre'],p['apellido'],p['documento'],p['direccion'],p['telefono'],p['correo_electronico']))
+                              values = (p['id_vehiculo'],p['id_cliente'],p['tipo_transaccion'],p['fecha'],p['monto'],p['observaciones']))
 
         self.tabla.heading('#0',text='ID')
-        self.tabla.heading('#1',text='ID de Transacción (número único y autoincremental)')
-        self.tabla.heading('#2',text='ID de Vehículo')
-        self.tabla.heading('#3',text='ID de Cliente')
-        self.tabla.heading('#4',text='Tipo de Transaccion(compra/Venta)')
-        self.tabla.heading('#5',text='Fecha')
-        self.tabla.heading('#6',text='Monto')
-        self.tabla.heading('#7',text='Observaciones')
+        self.tabla.heading('#1',text='ID de Vehículo')
+        self.tabla.heading('#2',text='ID de Cliente')
+        self.tabla.heading('#3',text='Tipo de Transaccion')
+        self.tabla.heading('#4',text='Fecha')
+        self.tabla.heading('#5',text='Monto')
+        self.tabla.heading('#6',text='Observaciones')
 
         self.tabla.column('#0', width=50)
         self.tabla.column('#1', width=100)
@@ -120,19 +162,15 @@ class Frame3(Frame):
         self.btn_editar.config(width= 20,font=('Arial', 12,'bold'),fg ='#FFFFFF' , bg='#1C500B',cursor='hand2',activebackground='#3FD83F',activeforeground='#000000')
         self.btn_editar.grid(row= 7, column=0,padx=10,pady=10, columnspan=2)
 
-        self.btn_delete = tk.Button(self, text='Borrar', command= self.borrar_cliente)
-        self.btn_delete.config(width= 20,font=('Arial', 12,'bold'),fg ='#FFFFFF' , bg='#A90A0A',cursor='hand2',activebackground='#F35B5B',activeforeground='#000000')
-        self.btn_delete.grid(row= 7, column=2,padx=10,pady=10, columnspan=2)
+        self.buscar_t = tk.StringVar()
+        self.buscar_t.set("example@example.com")
+        self.entry_buscar_t = tk.Entry(self,textvariable=self.buscar_t)
+        self.entry_buscar_t.config(width=30, font=('Arial',12))
+        self.entry_buscar_t.grid(row= 7, column=4,padx=10,pady=10, columnspan='2')
 
-        self.buscar_c = tk.StringVar()
-        self.buscar_c.set("example@example.com")
-        self.entry_buscar = tk.Entry(self,textvariable=self.buscar_c)
-        self.entry_buscar.config(width=30, font=('Arial',12))
-        self.entry_buscar.grid(row= 7, column=4,padx=10,pady=10, columnspan='2')
-
-        self.btn_buscar = tk.Button(self, text='Buscar', command= self.buscar_registro)
-        self.btn_buscar.config(width= 10,font=('Arial', 12,'bold'),fg ='#FFFFFF' , bg='#7B7B78',cursor='hand2',activebackground='#C6C6C0',activeforeground='#FFFFFF')
-        self.btn_buscar.grid(row= 7, column=7,padx=10,pady=10)
+        self.btn_buscar_t = tk.Button(self, text='Buscar')
+        self.btn_buscar_t.config(width= 10,font=('Arial', 12,'bold'),fg ='#FFFFFF' , bg='#7B7B78',cursor='hand2',activebackground='#C6C6C0',activeforeground='#FFFFFF')
+        self.btn_buscar_t.grid(row= 7, column=7,padx=10,pady=10)
 
 
     def editar_registro(self):
